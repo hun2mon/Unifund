d923ddb9fd3ea94f3c83ba2524af732d6515e895
HEO <sseungg95@gmail.com>
2024-04-18 17:16:37 +0900

    Project Create : 프로젝트가 3개인 상태에서 등록하면, 안내창 출력 / 개인정보처리 부분 제외 완료

--- src/main/java/com/uni/fund/project/controller/ProjectController.java
+++ src/main/java/com/uni/fund/project/controller/ProjectController.java
@@ -2,6 +2,7 @@ package com.uni.fund.project.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.uni.fund.project.dto.ProjectDTO;
 import com.uni.fund.project.service.ProjectService;
@@ -24,5 +28,32 @@ public class ProjectController {
 		model.addAttribute("project", project);
 		return "project/detail";
 	}
+	
+	@RequestMapping(value="/create")
+	public String projectCreateForm() {
+		logger.info(":: projectCreateForm IN ::");
+		return "project/create";
+	}
+	
+	@RequestMapping(value="/create", method=RequestMethod.POST)
+	public String projectCreate(MultipartFile pro_main_photo, MultipartFile pro_photo,@RequestParam Map<String, String> param, Integer mem_idx,Model model) {
+		logger.info(":: projectCreate CONTROLLER IN ::");
+		logger.info(":: Create param:{}",param);
+		String page = "project/create";
+		String msg = "등록실패";
+		mem_idx = 1;
+		
+		int row = projectService.projectCreate(pro_main_photo,pro_photo,param,mem_idx);
+		if(row == 1) {
+			page = "redirect:/";
+			msg = "등록완료되었습니다. 심사가 완료될때까지 기다려주세요.";
+
+		}
+		
+		model.addAttribute("msg",msg);
+		
+		return page;
+	}
+	
 	
 }
